4781ea228f9ae5f38b451097876228685c5b19ef
LuanGDantas <luan.dantas123@hotmail.com>
2022-06-09 00:17:42 -0300

    Fix: inclui a biblioteca 'cors'

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const routes = require('./routes');
+const cors = require('cors');
 
 require('./database');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 //app.use(errors()); 
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log('Server started!');
 });
